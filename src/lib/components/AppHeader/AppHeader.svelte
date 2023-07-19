8e4ed1605c1990cf4a871714102aa0e1eaffaec3
Edwards Presume <edwardspresume@gmail.com>
2023-07-19 16:09:59 -0400

    🔧 chore(AppHeader.svelte): reorganize imports and add missing newline at end of file ✨ feat(AppHeader.svelte): add navigation links to Prompts and Tags pages in the header 🎨 style(AppHeader.svelte): add styles for the active page link in the header

--- src/lib/components/AppHeader/AppHeader.svelte
+++ src/lib/components/AppHeader/AppHeader.svelte
@@ -1,10 +1,51 @@
 <script>
-    import Logo from "./Logo.svelte";
+    import { page } from '$app/stores';
 
+    import Logo from './Logo.svelte';
+    import ProfileMenu from './ProfileMenu.svelte';
 </script>
-<header>
 
+<header>
     <div>
         <Logo />
+
+        <nav class="flex items-center gap-4">
+            <ul class="flex gap-3">
+                <li>
+                    <a
+                        href="/"
+                        aria-label="Go to Prompts Page"
+                        aria-current={$page.url.pathname === '/'
+                            ? 'page'
+                            : undefined}
+                        class="transition-colors duration-200 hover:text-violet-600 dark:hover:text-violet-400"
+                        >Prompts</a
+                    >
+                </li>
+                <li>
+                    <a
+                        href="/tags"
+                        aria-label="Go to Tags Page"
+                        aria-current={$page.url.pathname === '/tags'
+                            ? 'page'
+                            : undefined}
+                        class="transition-colors duration-200 hover:text-violet-600 dark:hover:text-violet-400"
+                        >Tags</a
+                    >
+                </li>
+            </ul>
+
+            <ProfileMenu />
+        </nav>
     </div>
 </header>
+
+<style>
+    [aria-current='page'] {
+        color: rgb(110, 27, 255);
+    }
+
+    :global(.dark) [aria-current='page'] {
+        color: rgb(156, 104, 247);
+    }
+</style>

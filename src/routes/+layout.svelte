7876162e6e24959e6688b8c3f49f22b92b916587
Edwards Presume <edwardspresume@gmail.com>
2023-07-19 16:03:31 -0400

    🎨 style(layout.svelte): improve code formatting and add class attributes for main element to enhance styling and layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,9 @@
 <div class="h-[100dvh] flex flex-col pb-6">
     <AppHeader />
 
-    <main>
+    <main
+        class="relative grid w-full max-w-xl mx-auto mt-8 overflow-hidden pe-4 ps-4"
+    >
         <slot />
     </main>
 </div>

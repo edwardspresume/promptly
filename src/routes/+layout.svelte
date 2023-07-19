bf42053f044cebd497a3efbad100248dc0ed4c5f
Edwards Presume <edwardspresume@gmail.com>
2023-07-19 16:00:21 -0400

    🚀 feat(package.json): add "@vercel/analytics" package as a dependency to enable analytics tracking in the application
    🔧 chore(+layout.svelte): import and inject "@vercel/analytics" package to enable analytics tracking in the layout component
    🔧 chore(+layout.svelte): import and use "@zerodevx/svelte-toast" package to display toast notifications in the layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,22 @@
 <script>
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+
+    import { SvelteToast } from '@zerodevx/svelte-toast';
+
     import '../styles/app.postcss';
 
     import AppHeader from '$components/AppHeader.svelte';
+
+    inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<div>
+<div class="h-[100dvh] flex flex-col pb-6">
     <AppHeader />
+
     <main>
         <slot />
     </main>
 </div>
+
+<SvelteToast options={{ intro: { y: -100 } }} />

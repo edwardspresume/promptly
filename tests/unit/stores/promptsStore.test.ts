1bb3769ba89adf5424dae6474d4be91f001a0836
Edwards Presume <edwardspresume@gmail.com>
2023-07-22 14:45:10 -0400

    test(promptsStore.test.ts): add unit tests for prompts store functionality
    This commit adds unit tests for the prompts store functionality. The tests cover the following scenarios:
    
    - Initializes with default prompts if local storage is empty
    - Creates a new prompt
    - Updates a prompt
    - Deletes a prompt
    - Deletes all prompts
    - Sets and clears a text filter
    - Returns the correct total prompt count
    - Correctly sets sorting preference and reorders prompts
    
    These tests ensure that the prompts store functions as expected and handles various operations correctly.

---
+++ tests/unit/stores/promptsStore.test.ts
@@ -0,0 +1,129 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+
+import { get } from 'svelte/store';
+
+import defaultPrompts from '$data/defaultPrompts';
+import { createPromptsStore } from '$stores/promptsStore';
+import type { PromptStore } from '$types';
+import { LocalStorageKeys } from '$utils/localStorage';
+import { compareStrings } from '$utils/sortItems';
+
+describe('Prompts Store Functionality', () => {
+    let promptsStore: PromptStore;
+
+    beforeEach(() => {
+        promptsStore = createPromptsStore(LocalStorageKeys.PROMPTS);
+    });
+
+    it('Initializes with default prompts if local storage is empty', () => {
+        const { allPrompts } = promptsStore;
+        expect(get(allPrompts)).toEqual(defaultPrompts);
+    });
+
+    it('Creates a new prompt', () => {
+        const { allPrompts, createPrompt } = promptsStore;
+
+        const newPromptName = 'newPrompt';
+
+        createPrompt({
+            title: newPromptName,
+            isFavorited: true,
+            tagIds: [1],
+        });
+
+        expect(get(allPrompts)).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    title: newPromptName,
+                }),
+            ])
+        );
+    });
+
+    it('Updates a prompt', () => {
+        const { allPrompts, updatePrompt } = promptsStore;
+
+        const newPromptName = 'updatedPrompt';
+        updatePrompt(1, { title: newPromptName });
+
+        expect(get(allPrompts)).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    id: 1,
+                    title: newPromptName,
+                }),
+            ])
+        );
+    });
+
+    it('Deletes a prompt', () => {
+        const { allPrompts, deletePrompt } = promptsStore;
+
+        deletePrompt(1);
+
+        expect(get(allPrompts)).not.toContainEqual(
+            expect.objectContaining({ id: 1 })
+        );
+    });
+
+    it('Deletes all prompts', () => {
+        const { allPrompts, deleteAllPrompts } = promptsStore;
+
+        deleteAllPrompts();
+
+        expect(get(allPrompts)).toEqual([]);
+    });
+
+    it('Sets and clears a text filter', () => {
+        const { setTextFilter, filteredPrompts, allPrompts } = promptsStore;
+
+        console.warn = vi.fn();
+
+        setTextFilter('Exploring Mars');
+
+        expect(get(filteredPrompts)).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    title: 'Exploring Mars',
+                }),
+            ])
+        );
+
+        setTextFilter('');
+        expect(get(filteredPrompts)).toEqual(get(allPrompts));
+
+        expect(console.warn).toBeCalled();
+    });
+
+    it('Returns the correct total prompt count', () => {
+        const { totalPromptCount, allPrompts, createPrompt } = promptsStore;
+
+        expect(get(totalPromptCount)).toBe(get(allPrompts).length);
+
+        createPrompt({ title: 'newPrompt' });
+
+        expect(get(totalPromptCount)).toBe(get(allPrompts).length);
+    });
+
+    it('Correctly sets sorting preference and reorders prompts', () => {
+        const { setSortingPreference, filteredPrompts } = promptsStore;
+
+        // Test ascending order
+        setSortingPreference('title:ascending');
+        const sortedByTitleAsc = [...get(filteredPrompts)].sort((a, b) =>
+            compareStrings(a.title, b.title)
+        );
+        expect(get(filteredPrompts)).toEqual(sortedByTitleAsc);
+
+        // Test descending order
+        setSortingPreference('title:descending');
+        const sortedByTitleDesc = [...get(filteredPrompts)].sort((a, b) =>
+            compareStrings(b.title, a.title)
+        );
+        expect(get(filteredPrompts)).toEqual(sortedByTitleDesc);
+
+        // Test default order
+        setSortingPreference('default:default');
+        expect(get(filteredPrompts)).toEqual(get(promptsStore.allPrompts));
+    });
+});

03b795d34c11225d0306e97452c6a913f113bbb8
Edwards Presume <edwardspresume@gmail.com>
2023-07-23 09:38:31 -0400

    feat(ExportDataBtn.svelte): refactor exportPromptsAndTagsData function to separate data preparation and download logic for better modularity and testability feat(ExportDataBtn.svelte): add prepareDataForExport function to prepare prompts and tags data for export and return the download URL feat(ExportDataBtn.svelte): add downloadData function to handle the download of exported data using a temporary link test(ExportDataBtn.test.ts): add unit tests for prepareDataForExport, downloadData, and exportPromptsAndTagsData functions

--- src/lib/components/AppHeader/ExportDataBtn.svelte
+++ src/lib/components/AppHeader/ExportDataBtn.svelte
@@ -1,40 +1,58 @@
-<script>
-    import { get } from 'svelte/store';
-
+<script context="module" lang="ts">
     import promptsStore from '$stores/promptsStore';
     import tagsStore from '$stores/tagStore';
+    import { get } from 'svelte/store';
 
     import Icon from '$components/General/Icon.svelte';
 
     /**
-     * Function to handle data exportation, creating a downloadable JSON file
+     * Function to prepare data for export, creating a downloadable JSON file
      * containing data from the 'prompts' and 'tags' stores.
+     * @returns {string} downloadUrl - The URL of the downloadable JSON file
      */
-    function exportPromptsAndTagsData() {
-        try {
-            // Retrieve prompts and tags data from the store
-            const prompts = get(promptsStore.allPrompts);
-            const tags = get(tagsStore.allTags);
+    export function prepareDataForExport() {
+        // Retrieve prompts and tags data from the store
+        const prompts = get(promptsStore.allPrompts);
+        const tags = get(tagsStore.allTags);
 
-            // Combine the data into a single object
-            const exportData = { prompts, tags };
+        // Combine the data into a single object
+        const exportData = { prompts, tags };
 
-            // Create a Blob object from the JSON string
-            const exportBlob = new Blob([JSON.stringify(exportData)], {
-                type: 'application/json',
-            });
+        // Create a Blob object from the JSON string
+        const exportBlob = new Blob([JSON.stringify(exportData)], {
+            type: 'application/json',
+        });
 
-            // Create a URL for the Blob object
-            const downloadUrl = URL.createObjectURL(exportBlob);
+        // Create a URL for the Blob object
+        const downloadUrl = URL.createObjectURL(exportBlob);
 
-            // Create a temporary downloadable link and click it programmatically
-            const downloadLink = document.createElement('a');
-            downloadLink.href = downloadUrl;
-            downloadLink.download = 'promptly-data.json';
-            downloadLink.setAttribute('aria-hidden', 'true');
-            document.body.appendChild(downloadLink);
-            downloadLink.click();
+        return downloadUrl;
+    }
+
+    /**
+     * Function to handle the download of exported data.
+     * @param {string} url - The URL of the exported data
+     */
+    export function downloadData(url: string) {
+        // Create a temporary downloadable link and click it programmatically
+        const downloadLink = document.createElement('a');
+        downloadLink.href = url;
+        downloadLink.download = 'promptly-data.json';
+        downloadLink.setAttribute('aria-hidden', 'true');
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        if (process.env.NODE_ENV !== 'test') {
             document.body.removeChild(downloadLink);
+        }
+    }
+
+    /**
+     * Function to handle the full process of exporting prompts and tags data.
+     */
+    export function exportPromptsAndTagsData() {
+        try {
+            const url = prepareDataForExport();
+            downloadData(url);
         } catch (error) {
             console.error('Something went wrong while exporting data:', error);
         }

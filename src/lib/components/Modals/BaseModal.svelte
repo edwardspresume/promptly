73dca6b4e499e15941d39faf0369d089091eb31a
Edwards Presume <edwardspresume@gmail.com>
2023-07-28 20:17:29 -0400

    gpfix(BaseModal.svelte): fix class attribute to include dark mode styles for border color and background color

--- src/lib/components/Modals/BaseModal.svelte
+++ src/lib/components/Modals/BaseModal.svelte
@@ -11,7 +11,6 @@
     export let modalTitle: string;
 
     const dispatch = createEventDispatcher();
-
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -20,7 +19,7 @@
     on:close={() => dispatch('close')}
     on:click={(event) => closeDialogOnOutsideClick(event, dialogElement)}
     bind:this={dialogElement}
-    class="border rounded-lg border-white/30 bg-white/5 backdrop:backdrop-blur-md backdrop:bg-black/70 max-w-md max-h-[100dvh] overflow-y-auto"
+    class="border rounded-lg border-black/30 dark:border-white/30 bg-white dark:bg-white/5 backdrop:backdrop-blur-md backdrop:bg-black/70 max-w-md max-h-[100dvh] overflow-y-auto"
 >
     <header
         class="flex justify-between items-center mb-12 gap-7 text-[#b7b7b7]"

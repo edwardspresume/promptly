ca6db9a020a543a13fd0022fd60ed9e3b7dd0035
Edwards Presume <edwardspresume@gmail.com>
2023-08-03 16:25:44 -0400

    test(EmailAuthSchema.test.ts): add unit tests for EmailAuthSchema validation
    chore(tsconfig.json): add "types" configuration to include vitest/globals for type checking
    
    chore(vite.config.ts): enable globals in test configuration for sveltekit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     plugins: [sveltekit()],
     test: {
+        globals: true,
         environment: 'jsdom',
         include: ['tests/**/*.{test,spec}.{js,ts}'],
     },

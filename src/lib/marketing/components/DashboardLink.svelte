008d04b16c984a43c6f20c779dc28cbc4c424356
Edwards Presume <edwardspresume@gmail.com>
2023-08-20 17:32:51 -0400

    chore(ThemeToggleBtn.svelte): update transition duration to 300ms for smoother hover effect chore(DashboardLink.svelte): update transition duration to 300ms for smoother hover effect

--- src/lib/marketing/components/DashboardLink.svelte
+++ src/lib/marketing/components/DashboardLink.svelte
@@ -6,6 +6,6 @@
 <a
 	href="/dashboard/prompts"
 	aria-label="Go to the dashboard"
-	class="p-1 px-2 transition-all duration-200 rounded bg-primary hover:brightness-125 text-primary-foreground {extraStyles}"
+	class="p-1 px-2 transition-all duration-300 rounded bg-primary hover:brightness-125 text-primary-foreground {extraStyles}"
 	>{text}</a
 >

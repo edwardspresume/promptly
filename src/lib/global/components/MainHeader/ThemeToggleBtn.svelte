4a3fbe378ac3276950a10200bf0da42c02c03af3
Edwards Presume <edwardspresume@gmail.com>
2023-08-20 17:26:13 -0400

    feat(MainHeader): add ThemeToggleBtn component to toggle between dark and light mode fix(SiteHeader): adjust flex alignment to center items in the navigation menu fix(global.postcss): add transition property to body for smooth mode transition

---
+++ src/lib/global/components/MainHeader/ThemeToggleBtn.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import Icon from '$globalComponents/Icon.svelte';
+
+	// Property to control the visibility of the button text.
+	export let hideText: boolean = true;
+
+	//  Variable to store the dark mode state.
+	let isDarkMode: boolean = false;
+
+	/**
+	 * Function to toggle the dark mode of the application.
+	 * This function reads the current dark mode state, toggles it, and updates
+	 * the DOM and localStorage accordingly.
+	 */
+	function toggleDarkMode() {
+		isDarkMode = !isDarkMode;
+		localStorage.setItem('isDarkMode', JSON.stringify(isDarkMode));
+		document.documentElement.classList.toggle('dark', isDarkMode);
+	}
+
+	// Reactive statement to initialize the dark mode state from localStorage or system preference.
+	$: if (typeof window !== 'undefined') {
+		const storedValue = localStorage.getItem('isDarkMode');
+
+		isDarkMode = storedValue
+			? (JSON.parse(storedValue) as boolean)
+			: window.matchMedia('(prefers-color-scheme: dark)').matches;
+	}
+
+	$: iconName = isDarkMode ? 'sun' : 'moon';
+	$: buttonText = isDarkMode ? 'Switch to Light mode' : 'Switch to Dark mode';
+</script>
+
+<button
+	role="switch"
+	type="button"
+	aria-live="polite"
+	title={buttonText}
+	aria-label={buttonText}
+	aria-checked={isDarkMode}
+	on:click={toggleDarkMode}
+	class="p-1 transition-colors duration-200 rounded hover:bg-accent"
+>
+	<Icon name={iconName} />
+	{#if !hideText}
+		<span>{buttonText}</span>
+	{/if}
+</button>
--- src/lib/marketing/components/SiteHeader/sitePageLinks.svelte
+++ src/lib/marketing/components/SiteHeader/sitePageLinks.svelte
@@ -1,13 +1,18 @@
 <script>
+	import ThemeToggleBtn from '$globalComponents/MainHeader/ThemeToggleBtn.svelte';
 	import DashboardLink from '$marketingComponents/DashboardLink.svelte';
 </script>
 
-<ul class="flex gap-4">
+<ul class="flex items-center gap-4">
 	<li>
 		<DashboardLink />
 	</li>
 
 	<li>
 		<a href="/auth" aria-label="Sign up for a new account">Sign up</a>
+	</li>
+
+	<li class="flex -ml-2">
+		<ThemeToggleBtn />
 	</li>
 </ul>

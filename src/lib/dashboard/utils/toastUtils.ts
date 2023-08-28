3a95d29ec1c8009b9a6b7d0f72f49b57784af5e4
Edwards Presume <edwardspresume@gmail.com>
2023-08-28 14:46:32 -0400

    feat(toastUtils.ts): add toastUtils module for displaying success and error toast notifications
    feat(toastUtils.ts): implement notifySuccess function to show success toast notifications
    
    feat(toastUtils.ts): implement notifyError function to show error toast notifications
    
    feat(toastUtils.ts): implement removeLastToast function to remove the last toast targeted to 'baseModal'
    
    feat(toast.postcss): add toast component styles for toast notifications
    
    feat(global.postcss): import toast component styles for global styles
    
    The changes were made to add a new module called toastUtils.ts that provides functions for displaying success and error toast notifications. The notifySuccess function is implemented to show a success toast notification with a custom message. The notifyError function is implemented to show an error toast notification with a custom message. The removeLastToast function is implemented to remove the last toast targeted to 'baseModal'. Additionally, toast component styles are added in toast.postcss and imported in global.postcss to style the toast notifications.

---
+++ src/lib/dashboard/utils/toastUtils.ts
@@ -0,0 +1,42 @@
+/**
+ * Toast notifications utility for app
+ * This module provides functions to show success and error toast notifications.
+ * @module toastUtils
+ */
+
+import { toast } from '@zerodevx/svelte-toast';
+
+/**
+ * Creates the HTML content for the toast notification.
+ * @param {string} iconClass - The CSS class for the icon to be displayed in the toast.
+ * @param {string} message - The message to be displayed in the toast.
+ * @returns {string} - The HTML content for the toast.
+ */
+function createToastContent(iconClass: string, message: string) {
+	return `<div class="${iconClass}"></div><span>${message}</span>`;
+}
+
+/**
+ * Shows a success toast notification.
+ * @param {string} message - The message to be displayed in the toast.
+ * @param {object} [options={}] - Optional configuration for the toast.
+ */
+export const notifySuccess = (message: string, options: Record<string, unknown> = {}) => {
+	const content = createToastContent('check_mark', message);
+	toast.push(content, options);
+};
+
+/**
+ * Shows an error toast notification.
+ * @param {string} message - The message to be displayed in the toast.
+ * @param {object} [options={}] - Optional configuration for the toast.
+ */
+export const notifyError = (message: string, options: Record<string, unknown> = {}) => {
+	const content = createToastContent('error_mark', message);
+	toast.push(content, options);
+};
+
+/**
+ * Removes the last toast targeted to 'baseModal'.
+ */
+export const removeLastToast = () => toast.pop({ target: 'baseModal' });

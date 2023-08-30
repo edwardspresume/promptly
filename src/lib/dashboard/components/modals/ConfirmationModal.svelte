67f38aa7611c4c021edbe30c93fd308f327ce8fa
Edwards Presume <edwardspresume@gmail.com>
2023-08-30 14:18:41 -0400

    feat(ConfirmationModal.svelte): add ConfirmationModal component to handle confirmation dialogs
    The ConfirmationModal component is added to handle confirmation dialogs in the dashboard. It allows users to confirm or cancel an action before proceeding.
    
    The component imports necessary functions and types from various modules. It receives props such as `confirmationModalRef` and `confirmationInfo` to control the behavior and content of the modal.
    
    The component includes a script section that updates the heading, subheading, toastMessage, and callback when the `confirmationInfo` prop changes. It also includes a function `executeCallbackAndCloseModal` that executes the callback, closes the modal dialog, and displays a success notification if `toastMessage` is provided.
    
    The template section includes the HTML structure of the modal dialog, including the heading, subheading, and buttons for cancel and confirm actions. The `closeDialogOnOutsideClick` function is used to close the modal when clicking outside of it.
    
    This new component enhances the user experience by providing a standardized way to handle confirmation dialogs in the dashboard.

---
+++ src/lib/dashboard/components/modals/ConfirmationModal.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { invalidateAll } from '$app/navigation';
+	import type { ConfirmationInfo } from '$dashboardTypes';
+	import { closeDialogOnOutsideClick } from '$dashboardUtils/functions';
+	import { notifySuccess } from '$dashboardUtils/toastUtils';
+	import Button from '$globalComponents/ui/button/button.svelte';
+
+	export let confirmationModalRef: HTMLDialogElement;
+	export let confirmationInfo: ConfirmationInfo;
+
+	let heading: ConfirmationInfo['heading'];
+	let subheading: ConfirmationInfo['subheading'];
+	let toastMessage: ConfirmationInfo['toastMessage'];
+	let callback: ConfirmationInfo['callback'];
+
+	/**
+	 * Updates heading, callback, and toastMessage when confirmationInfo changes
+	 */
+	$: if (confirmationInfo) {
+		({ heading, subheading, toastMessage, callback } = confirmationInfo);
+	}
+
+	/**
+	 * Executes the callback, closes the modal dialog, and displays a success notification
+	 */
+	async function executeCallbackAndCloseModal() {
+		await callback();
+
+		await invalidateAll();
+
+		confirmationModalRef.close();
+
+		if (toastMessage) notifySuccess(toastMessage);
+	}
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<dialog
+	on:click={(event) => closeDialogOnOutsideClick(event, confirmationModalRef)}
+	bind:this={confirmationModalRef}
+>
+	<header>
+		<h3 class="text-xl">{heading}</h3>
+
+		<p class="mt-1 text-sm text-muted-foreground">{@html subheading}</p>
+	</header>
+
+	<footer class="flex justify-end gap-3 mt-6">
+		<Button type="button" variant="outline" on:click={() => confirmationModalRef.close()}>
+			Cancel
+		</Button>
+
+		<Button type="button" on:click={executeCallbackAndCloseModal}>Confirm</Button>
+	</footer>
+</dialog>

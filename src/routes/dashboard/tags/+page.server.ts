a2b0cbabb0de6ae12603d184ab46dd03760be753
Edwards Presume <edwardspresume@gmail.com>
2023-08-31 15:52:56 -0400

    refactor(tags/+page.server.ts): change the name of the createTag action to default to write less code feat(tags/+page.server.ts): add support for updating a tag if tagId is provided feat(tags/+page.server.ts): improve error message to include whether it was a tag creation or update feat(tags/+page.server.ts): improve success message to include whether it was a tag creation or update

--- src/routes/dashboard/tags/+page.server.ts
+++ src/routes/dashboard/tags/+page.server.ts
@@ -1,4 +1,6 @@
 import { drizzleClient } from '$databaseDir/drizzleClient.server';
+import { eq } from 'drizzle-orm';
+
 import { message, superValidate } from 'sveltekit-superforms/server';
 
 import type { Actions, PageServerLoad } from './$types';
@@ -14,7 +16,7 @@ export const load = (async () => {
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
-	createTag: async ({ request, locals: { getSession } }) => {
+	default: async ({ request, locals: { getSession } }) => {
 		const tagForm = await superValidate<typeof TagValidationSchema, FormStatusMessage>(
 			request,
 			TagValidationSchema
@@ -26,29 +28,35 @@ export const actions: Actions = {
 				text: 'The tag name you entered is invalid. Please enter a valid tag name.'
 			});
 
+		const tagId = tagForm.data.id;
 		const session = await getSession();
 
 		if (session) {
-			const formData = {
-				userId: session.user.id,
-				name: tagForm.data.name
-			};
-
 			try {
-				await drizzleClient.insert(tagsTable).values([formData]);
+				if (tagId) {
+					await drizzleClient
+						.update(tagsTable)
+						.set({ name: tagForm.data.name })
+						.where(eq(tagsTable.id, tagId));
+				} else {
+					await drizzleClient.insert(tagsTable).values({
+						userId: session.user.id,
+						name: tagForm.data.name
+					});
+				}
 			} catch (error) {
 				console.error(error);
 
 				return message(tagForm, {
 					statusType: 'error',
-					text: 'Unexpected error during tag creation. Please retry.'
+					text: `Unexpected error during tag ${tagId ? 'update' : 'creation'}. Please retry.`
 				});
 			}
 		}
 
 		return message(tagForm, {
 			statusType: 'success',
-			text: 'Tag created successfully!'
+			text: `Tag ${tagId ? 'updated' : 'created'} successfully!`
 		});
 	}
 };

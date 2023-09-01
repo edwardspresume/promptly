e538515e9e3695de958503516593b013b09f0059
Edwards Presume <edwardspresume@gmail.com>
2023-09-01 07:45:27 -0400

    feat(FilterDisplayButton.svelte): add FilterDisplayButton component to display and trigger event to show filters
    The FilterDisplayButton component is added to the project. It imports the Button component from the global components library and the createEventDispatcher function from the svelte package. The component creates an event dispatcher using the createEventDispatcher function.
    
    The FilterDisplayButton component renders a Button component with the following properties:
    - type: "button"
    - variant: "outline"
    - title: "Show Filters"
    - aria-label: "Show Filters"
    
    When the button is clicked, it triggers the 'showFilters' event using the dispatch function.
    
    This component is added to provide a button to display the filters and trigger the event to show the filters when clicked.

---
+++ src/lib/dashboard/components/filters/FilterDisplayButton.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Button from '$globalComponents/ui/button/button.svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<Button
+	type="button"
+	variant="outline"
+	title="Show Filters"
+	aria-label="Show Filters"
+	on:click={() => dispatch('showFilters')}
+>
+	Filters
+</Button>

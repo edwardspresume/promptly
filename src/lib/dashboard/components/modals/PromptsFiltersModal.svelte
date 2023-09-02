37b8f51b7efd3b7c61c3873f308f1a88ba5de761
Edwards Presume <edwardspresume@gmail.com>
2023-09-02 10:09:23 -0400

    feat(PromptsFiltersModal.svelte): add PromptsFiltersModal component to handle filtering prompts feat(PromptsFiltersModal.svelte): implement clearFilters function to reset selected filters feat(PromptsFiltersModal.svelte): add SortSelector component to select sorting option for prompts feat(PromptsFiltersModal.svelte): add TagSelector component to select tags for filtering prompts feat(PromptsFiltersModal.svelte): add Clear Filters button to reset all selected filters feat(PromptsFiltersModal.svelte): add animation to slide in the modal when it appears

---
+++ src/lib/dashboard/components/modals/PromptsFiltersModal.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import { writable } from 'svelte/store';
+
+	import { closeDialogOnOutsideClick } from '$dashboardUtils/functions';
+
+	import { promptSortOptions } from '$dashboardData/SortOptions';
+
+	import { promptSortingPreference } from '$dashboardStores/promptsStore';
+	import { totalTagsCountStore } from '$dashboardStores/tagsStore';
+
+	import SortSelector from '$dashboardComponents/filters/SortSelector.svelte';
+	import TagSelector from '$dashboardComponents/filters/TagSelector.svelte';
+	import Button from '$globalComponents/ui/button/button.svelte';
+	import CloseModalBtn from './CloseModalBtn.svelte';
+
+	export let promptsFiltersModalRef: HTMLDialogElement;
+
+	let selectedTagIds = writable<string[]>([]);
+	let selectedSortOption: string;
+
+	/**
+	 * Clears all the selected filters
+	 * Resets the selected tag ids to empty array and sort option to empty string.
+	 */
+	function clearFilters() {
+		if ($selectedTagIds.length > 0) selectedTagIds.set([]);
+
+		selectedSortOption = '';
+		promptSortingPreference.set('');
+	}
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<dialog
+	bind:this={promptsFiltersModalRef}
+	on:click={(event) => closeDialogOnOutsideClick(event, promptsFiltersModalRef)}
+	class="w-3/4 h-full max-w-sm max-h-full m-0 ml-auto rounded-none backdrop:backdrop-blur-none"
+>
+	<header class="flex items-center justify-between pb-2 mb-8">
+		<h3 class="text-lg">Filters</h3>
+		<CloseModalBtn dialogElement={promptsFiltersModalRef} />
+	</header>
+
+	<div class="grid gap-5">
+		<SortSelector itemType="prompt" sortOptions={promptSortOptions} bind:selectedSortOption />
+
+		{#if $totalTagsCountStore}
+			<TagSelector {selectedTagIds} filterPromptBasedOnTags={true} />
+		{/if}
+
+		<Button type="button" on:click={clearFilters}>Clear Filters</Button>
+	</div>
+</dialog>
+
+<style>
+	dialog {
+		animation: slideInKeyframe 0.4s ease-in-out;
+	}
+
+	@keyframes slideInKeyframe {
+		from {
+			opacity: 0;
+			transform: translateX(100%);
+		}
+		to {
+			opacity: 1;
+			transform: translateX(0);
+		}
+	}
+</style>

d1365588f8cd937fac8f4b4f8ab960c32df9291f
Edwards Presume <edwardspresume@gmail.com>
2023-09-04 10:44:27 -0400

    refactor(AccountPopUpMenu.svelte): Remove unused imports and update Avatar component usage refactor(AccountPopUpMenu.svelte): Remove unused Avatar component files and index export

--- src/lib/dashboard/components/dashboardHeader/AccountPopUpMenu.svelte
+++ src/lib/dashboard/components/dashboardHeader/AccountPopUpMenu.svelte
@@ -1,7 +1,6 @@
 <script>
 	import { userProfileStore } from '$dashboardStores/userProfileStore';
 
-	import * as Avatar from '$globalComponents/ui/avatar';
 	import * as Popover from '$globalComponents/ui/popover';
 	import AccountPopUpMenuItems from './AccountPopUpMenuItems.svelte';
 
@@ -11,16 +10,15 @@
 </script>
 
 <Popover.Root>
-	<Popover.Trigger type="button" class="rounded-full">
-		<Avatar.Root>
-			{#if userAvatarUrl}
-				<Avatar.Image src={userAvatarUrl} alt="Profile avatar" />
-			{:else}
-				<Avatar.Fallback class="bg-gradient-to-b from-blue-500 to-purple-500" style="display:flex">
-					{fullNameInitial || emailInitial}
-				</Avatar.Fallback>
-			{/if}
-		</Avatar.Root>
+	<Popover.Trigger
+		type="button"
+		class="w-10 h-10 rounded-full bg-gradient-to-b from-blue-500 to-purple-500"
+	>
+		{#if userAvatarUrl}
+			<img src={userAvatarUrl} alt="Profile avatar" class="w-full h-full rounded-full" />
+		{:else}
+			{fullNameInitial || emailInitial}
+		{/if}
 	</Popover.Trigger>
 
 	<Popover.Content class="p-2 mt-2 w-fit">

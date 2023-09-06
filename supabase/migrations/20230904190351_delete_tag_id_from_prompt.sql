fbfe02f84a9c3a650c15474b1e4557a1c0c1920d
Edwards Presume <edwardspresume@gmail.com>
2023-09-05 20:49:23 -0400

    refactor: reset DB and supabase init

--- supabase/migrations/20230904190351_delete_tag_id_from_prompt.sql
+++
@@ -1,7 +0,0 @@
-drop trigger if exists "remove_tag_id_from_prompts_trigger" on "public"."tags";
-
-drop function if exists "public"."remove_tag_id_from_prompts"();
-
-alter table "public"."prompts" alter column "tag_ids" set not null;
-
-

6342ec818a879d1e8706b12899b1a37cd90b351a
Edwards Presume <edwardspresume@gmail.com>
2023-09-11 14:02:43 -0400

    removed unneeded section element

--- src/routes/dashboard/prompts/[id]/+page.svelte
+++ src/routes/dashboard/prompts/[id]/+page.svelte
@@ -20,56 +20,46 @@
 	$: isLoggedIn = data.session?.user;
 </script>
 
-<section class="grid w-full place-items-center">
-	<Card.Root
-		role="article"
-		aria-label="Shared Prompt Details"
-		class="w-[96%] max-w-lg bg-background"
-	>
-		<Card.Header>
-			<Card.Title>Shared Prompt</Card.Title>
-			<Card.Description>Created by {sharedPrompt.fromUser}</Card.Description>
-		</Card.Header>
+<Card.Root role="article" aria-label="Shared Prompt Details" class="w-[96%] max-w-lg bg-background">
+	<Card.Header>
+		<Card.Title>Shared Prompt</Card.Title>
+		<Card.Description>Created by {sharedPrompt.fromUser}</Card.Description>
+	</Card.Header>
 
-		<Card.Content class="grid gap-5">
-			<InputField type="text" label="Title" value={sharedPrompt.title} readonly />
+	<Card.Content class="grid gap-5">
+		<InputField type="text" label="Title" value={sharedPrompt.title} readonly />
 
-			<fieldset class="grid gap-1">
-				<TextArea rows="9" label="Description" value={sharedPrompt.description} readonly />
-			</fieldset>
-		</Card.Content>
+		<fieldset class="grid gap-1">
+			<TextArea rows="9" label="Description" value={sharedPrompt.description} readonly />
+		</fieldset>
+	</Card.Content>
 
-		<Card.Footer>
-			<form
-				method="post"
-				use:enhance={() => {
-					isSaving = true;
+	<Card.Footer>
+		<form
+			method="post"
+			use:enhance={() => {
+				isSaving = true;
 
-					return async ({ update, result }) => {
-						await update();
-						isSaving = false;
+				return async ({ update, result }) => {
+					await update();
+					isSaving = false;
 
-						const { alertText, alertType } = result.data;
+					const { alertText, alertType } = result.data;
 
-						const notificationFunction = getNotificationFunction(alertType);
+					const notificationFunction = getNotificationFunction(alertType);
 
-						notificationFunction(alertText, { target: 'dashboardLayout' });
-					};
-				}}
-			>
-				<SubmitButton
-					title="Save Prompt"
-					showSpinner={isSaving}
-					disabled={isSaving || !isLoggedIn}
-				/>
-			</form>
-		</Card.Footer>
-	</Card.Root>
+					notificationFunction(alertText, { target: 'dashboardLayout' });
+				};
+			}}
+		>
+			<SubmitButton title="Save Prompt" showSpinner={isSaving} disabled={isSaving || !isLoggedIn} />
+		</form>
+	</Card.Footer>
+</Card.Root>
 
-	{#if !isLoggedIn}
-		<p class="mt-5">
-			<Button variant="link" href={RoutePaths.AUTH} class="p-0">Sign in</Button> to save this prompt
-			to your account
-		</p>
-	{/if}
-</section>
+{#if !isLoggedIn}
+	<p class="mt-5 italic font-medium text-accent-foreground">
+		<Button variant="link" href={RoutePaths.AUTH} class="p-0">Sign in</Button> to save this prompt to
+		your account
+	</p>
+{/if}

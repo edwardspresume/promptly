bc3e2831e5da33e36322ea3d06e02040a0b6c048
Edwards Presume <edwardspresume@gmail.com>
2023-09-11 12:18:24 -0400

    feat(+page.server.ts): add server-side action to handle saving shared prompt data to the database feat(+page.svelte): add form submission logic to save prompt data and display success/error notifications

--- src/routes/dashboard/prompts/[id]/+page.server.ts
+++ src/routes/dashboard/prompts/[id]/+page.server.ts
@@ -1,7 +1,11 @@
-import { error } from '@sveltejs/kit';
+import { error, fail, redirect, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
+import { drizzleClient } from '$databaseDir/drizzleClient.server';
+import { promptsTable } from '$databaseDir/schema';
 import { getPromptById } from '$databaseDir/utils.server';
+import { RoutePaths, type AlertMessage } from '$globalTypes';
+import { logError } from '$globalUtils';
 
 export const load = (async ({ params }) => {
 	const { id: promptId } = params;
@@ -18,3 +22,48 @@ export const load = (async ({ params }) => {
 		}
 	};
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	default: async ({ params, locals: { getSession } }) => {
+		const userSession = (await getSession())?.user;
+
+		if (!userSession) return;
+
+		const { id: promptId } = params;
+
+		if (!promptId) throw error(404, 'Prompt id not provided');
+
+		const promptData = await getPromptById(promptId);
+
+		let alertMessage: AlertMessage;
+
+		if (!promptData) {
+			alertMessage = {
+				alertType: 'error',
+				alertText: 'Prompt not found'
+			};
+
+			return fail(400, alertMessage);
+		}
+
+		try {
+			await drizzleClient.insert(promptsTable).values({
+				profileId: userSession.id,
+				title: promptData.title,
+				description: promptData.description,
+				tagIds: []
+			});
+		} catch (error) {
+			logError(error, 'Error in saving shared prompt', { userSession });
+
+			alertMessage = {
+				alertType: 'error',
+				alertText: 'Error in saving shared prompt. Please try again.'
+			};
+
+			return fail(400, alertMessage);
+		}
+
+		throw redirect(303, RoutePaths.DASHBOARD_PROMPTS);
+	}
+};
--- src/routes/dashboard/prompts/[id]/+page.svelte
+++ src/routes/dashboard/prompts/[id]/+page.svelte
@@ -1,32 +1,75 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import type { PageData } from './$types';
 
-	import Button from '$globalComponents/ui/button/button.svelte';
-	import * as Card from '$globalComponents/ui/card';
+	import { getNotificationFunction } from '$dashboardUtils/toastUtils';
 
 	import InputField from '$globalComponents/form/InputField.svelte';
+	import SubmitButton from '$globalComponents/form/SubmitButton.svelte';
 	import TextArea from '$globalComponents/form/TextArea.svelte';
+	import Button from '$globalComponents/ui/button/button.svelte';
+	import * as Card from '$globalComponents/ui/card';
+	import { RoutePaths } from '$globalTypes';
 
 	export let data: PageData;
 
 	const { sharedPrompt } = data;
+
+	let isSaving = false;
+
+	$: isLoggedIn = data.session?.user;
 </script>
 
-<Card.Root role="article" aria-label="Shared Prompt Details" class="w-[96%] max-w-lg bg-background">
-	<Card.Header>
-		<Card.Title>Shared Prompt</Card.Title>
-		<Card.Description>Created by {sharedPrompt.fromUser}</Card.Description>
-	</Card.Header>
+<section class="grid w-full place-items-center">
+	<Card.Root
+		role="article"
+		aria-label="Shared Prompt Details"
+		class="w-[96%] max-w-lg bg-background"
+	>
+		<Card.Header>
+			<Card.Title>Shared Prompt</Card.Title>
+			<Card.Description>Created by {sharedPrompt.fromUser}</Card.Description>
+		</Card.Header>
 
-	<Card.Content class="grid gap-5">
-		<InputField type="text" label="Title" value={sharedPrompt.title} readonly />
+		<Card.Content class="grid gap-5">
+			<InputField type="text" label="Title" value={sharedPrompt.title} readonly />
 
-		<fieldset class="grid gap-1">
-			<TextArea label="Description" value={sharedPrompt.description} readonly />
-		</fieldset>
-	</Card.Content>
+			<fieldset class="grid gap-1">
+				<TextArea rows="9" label="Description" value={sharedPrompt.description} readonly />
+			</fieldset>
+		</Card.Content>
 
-	<Card.Footer>
-		<Button>Save Prompt</Button>
-	</Card.Footer>
-</Card.Root>
+		<Card.Footer>
+			<form
+				method="post"
+				use:enhance={() => {
+					isSaving = true;
+
+					return async ({ update, result }) => {
+						await update();
+						isSaving = false;
+
+						const { alertText, alertType } = result.data;
+
+						const notificationFunction = getNotificationFunction(alertType);
+
+						notificationFunction(alertText, { target: 'dashboardLayout' });
+					};
+				}}
+			>
+				<SubmitButton
+					title="Save Prompt"
+					showSpinner={isSaving}
+					disabled={isSaving || !isLoggedIn}
+				/>
+			</form>
+		</Card.Footer>
+	</Card.Root>
+
+	{#if !isLoggedIn}
+		<p class="mt-5">
+			<Button variant="link" href={RoutePaths.AUTH} class="p-0">Sign in</Button> to save this prompt
+			to your account
+		</p>
+	{/if}
+</section>

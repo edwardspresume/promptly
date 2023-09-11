8bd9cf840a62938cc558a1f4e1fab2d8fa50c38d
Edwards Presume <edwardspresume@gmail.com>
2023-09-11 17:39:25 -0400

    refactor: wrapped label around selector and improved appearance and styling

--- src/lib/dashboard/components/filters/TagSelector.svelte
+++ src/lib/dashboard/components/filters/TagSelector.svelte
@@ -16,6 +16,7 @@
 
 	// A writable store that keeps track of the IDs of selected tags
 	export let selectedTagIds: Writable<string[]>;
+	export let labelIsScreenReaderOnly: boolean = false;
 
 	let tagSearchInput: HTMLInputElement;
 
@@ -83,59 +84,67 @@
 
 	// Sets tag filter in prompts store when filterPromptBasedOnTags is true
 	$: if (filterPromptBasedOnTags) tagsFilter.set($selectedTagIds);
+
+	let tagLabel = `Tag${$allTagsStore.length > 1 ? 's' : ''}`;
 </script>
 
-<section
-	use:onOutsideClick={() => (isTagSelectionMenuOpen = false)}
-	class="relative grid gap-3 p-2 border rounded-md bg-background"
-	aria-label="Tag selection form"
->
-	{#if selectedTags.length > 0}
-		<div class="flex flex-wrap gap-2">
-			{#each selectedTags as tag (tag.id)}
-				<SelectedTag {tag} on:removeTag={() => removeTag(tag)} />
-			{/each}
-		</div>
-	{/if}
+<label for="tagInput" class="grid gap-1">
+	<span
+		class="text-sm font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70 {labelIsScreenReaderOnly
+			? 'sr-only'
+			: ''}"
+	>
+		{tagLabel}
+	</span>
 
-	<label>
-		<span class="sr-only">Select tags</span>
-		<input
-			bind:value={tagSearchQuery}
-			bind:this={tagSearchInput}
-			on:keydown={handleKeyDown}
-			on:focus={() => (isTagSelectionMenuOpen = true)}
-			on:input={() => (activeTagIndex = 0)}
-			type="search"
-			role="combobox"
-			autocorrect="off"
-			autocomplete="off"
-			spellcheck="false"
-			aria-expanded={isTagSelectionMenuOpen}
-			aria-autocomplete="list"
-			aria-controls="tags-list"
-			placeholder="Select tags"
-			aria-activedescendant={`tag-${activeTagIndex}`}
-			class="w-full px-3 py-2 text-sm bg-transparent border rounded-md border-input ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-		/>
-	</label>
+	<div use:onOutsideClick={() => (isTagSelectionMenuOpen = false)} class="relative grid gap-3">
+		{#if selectedTags.length > 0}
+			<div class="flex flex-wrap gap-2 p-2 border rounded-md bg-background">
+				{#each selectedTags as tag (tag.id)}
+					<SelectedTag {tag} on:removeTag={() => removeTag(tag)} />
+				{/each}
+			</div>
+		{/if}
 
-	{#if isTagSelectionMenuOpen && filteredTags.length > 0}
-		<fieldset
-			id="tags-list"
-			aria-label="Tags list"
-			class="gap-3 p-2 overflow-y-auto border rounded-md max-h-32 sm:max-h-52"
-		>
-			{#each filteredTags as tag, index (tag.id)}
-				<Tag
-					{tag}
-					{index}
-					isActive={index === activeTagIndex}
-					on:addTag={() => addTag(tag)}
-					on:focus={() => (activeTagIndex = index)}
-					on:hover={() => (activeTagIndex = index)}
-				/>
-			{/each}
-		</fieldset>
-	{/if}
-</section>
+		<label>
+			<span class="sr-only">Select tags</span>
+			<input
+				bind:value={tagSearchQuery}
+				bind:this={tagSearchInput}
+				on:keydown={handleKeyDown}
+				on:focus={() => (isTagSelectionMenuOpen = true)}
+				on:input={() => (activeTagIndex = 0)}
+				type="search"
+				role="combobox"
+				autocorrect="off"
+				autocomplete="off"
+				spellcheck="false"
+				aria-expanded={isTagSelectionMenuOpen}
+				aria-autocomplete="list"
+				aria-controls="tags-list"
+				placeholder="Select {tagLabel.toLowerCase()}"
+				aria-activedescendant={`tag-${activeTagIndex}`}
+				class="w-full px-3 py-2 text-sm bg-transparent border rounded-md border-input ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+			/>
+		</label>
+
+		{#if isTagSelectionMenuOpen && filteredTags.length > 0}
+			<fieldset
+				id="tags-list"
+				aria-label="Tags list"
+				class="gap-3 p-2 overflow-y-auto border rounded-md max-h-32 sm:max-h-52"
+			>
+				{#each filteredTags as tag, index (tag.id)}
+					<Tag
+						{tag}
+						{index}
+						isActive={index === activeTagIndex}
+						on:addTag={() => addTag(tag)}
+						on:focus={() => (activeTagIndex = index)}
+						on:hover={() => (activeTagIndex = index)}
+					/>
+				{/each}
+			</fieldset>
+		{/if}
+	</div>
+</label>

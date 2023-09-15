cb5a10122053d818a56b6b730cef067d71aaeb84
Edwards Presume <edwardspresume@gmail.com>
2023-09-15 08:53:14 -0400

    fix: wrap elements in a <section> tag so that the nested element are not far apart

--- src/routes/dashboard/prompts/[id]/+error.svelte
+++ src/routes/dashboard/prompts/[id]/+error.svelte
@@ -2,5 +2,7 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1 class="text-6xl">Error {$page.status}</h1>
-<p class="mt-5 text-2xl">{$page.error?.message || 'An unexpected error occurred'}</p>
+<section class="text-center">
+	<h1 class="text-6xl">Error {$page.status}</h1>
+	<p class="mt-5 text-2xl">{$page.error?.message || 'An unexpected error occurred'}</p>
+</section>

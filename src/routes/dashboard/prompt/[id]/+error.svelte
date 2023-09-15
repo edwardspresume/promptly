cb27865c781bdd1ec7086d82cd7eab0ed1315e55
Edwards Presume <edwardspresume@gmail.com>
2023-09-15 16:59:59 -0400

    style(+error.svelte): change section element to main element and add grid and place-content-center classes to improve layout and center content

--- src/routes/dashboard/prompt/[id]/+error.svelte
+++ src/routes/dashboard/prompt/[id]/+error.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 </script>
 
-<section class="text-center">
+<main class="grid h-full text-center place-content-center">
 	<h1 class="text-6xl">Error {$page.status}</h1>
 	<p class="mt-5 text-2xl">{$page.error?.message || 'An unexpected error occurred'}</p>
-</section>
+</main>

221b61c42d88e59b18f2e66a5bd213a8664e4d4d
Edwards Presume <edwardspresume@gmail.com>
2023-09-16 12:21:32 -0400

    fix(LoginLogoutBtn.svelte): change "Login" to "Log in" for better consistency and clarity feat(LoginLogoutBtn.svelte): add call to invalidateAll() after signing out to ensure data is refreshed

--- src/lib/dashboard/components/dashboardHeader/LoginLogoutBtn.svelte
+++ src/lib/dashboard/components/dashboardHeader/LoginLogoutBtn.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { invalidateAll } from '$app/navigation';
 	import { page } from '$app/stores';
 
 	import { RoutePaths } from '$globalTypes';
@@ -7,7 +8,11 @@
 
 	import Icon from '$globalComponents/Icon.svelte';
 
-	const handleSignOut = async () => await $page.data.supabase?.auth.signOut();
+	const handleSignOut = async () => {
+		await $page.data.supabase?.auth.signOut();
+
+		invalidateAll();
+	};
 
 	$: isLoggedIn = Boolean($userProfileStore);
 </script>
@@ -20,6 +25,6 @@
 {:else}
 	<a href={RoutePaths.AUTH} aria-label="Go to the sign in page">
 		<Icon name="login" />
-		<span>Login</span>
+		<span>Log in</span>
 	</a>
 {/if}

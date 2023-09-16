80652ce2e022cce83235dcbc963f65942b1d2e80
Edwards Presume <edwardspresume@gmail.com>
2023-09-16 16:46:52 -0400

    refactor: moved the error page to the base of dashboard route so that it can be used for  all the errors that occur in this route

---
+++ src/routes/dashboard/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	import { RoutePaths } from '$globalTypes';
+
+	import Button from '$globalComponents/ui/button/button.svelte';
+</script>
+
+<main class="grid h-full gap-3 justify-items-center place-content-center">
+	{#if $page.status === 401}
+		<h1 class="text-2xl">Authentication required</h1>
+
+		<p>{$page.error?.message}</p>
+
+		<div class="flex gap-2">
+			<Button href={RoutePaths.AUTH}>Log in</Button>
+
+			<Button href={RoutePaths.SIGNUP}>Sign up</Button>
+		</div>
+	{:else}
+		<h1 class="text-2xl">Error</h1>
+
+		<p>{$page.error?.message}</p>
+	{/if}
+</main>

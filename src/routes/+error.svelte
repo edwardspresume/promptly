3b8bb3d560e0e9a4905f633dda202207142883ff
Edwards Presume <edwardspresume@gmail.com>
2023-09-18 10:12:41 -0400

    chore(package.json): update prettier and prettier-plugin-svelte versions to 3.0.3 for compatibility and bug fixes feat(+error.svelte): add error page component to handle 404 errors and display appropriate messages and buttons for navigation

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	import { RoutePaths } from '$globalTypes';
+
+	import Button from '$globalComponents/ui/button/button.svelte';
+</script>
+
+<main class="grid h-[100dvh] gap-8 place-content-center">
+	{#if $page.status === 404}
+		<h1 class="text-4xl text-center">Page not found</h1>
+
+		<div class="flex flex-wrap justify-center gap-3">
+			<Button href={RoutePaths.MARKETING_LANDING}>Return to homepage</Button>
+
+			<Button href={RoutePaths.DASHBOARD_PROMPTS}>Return to dashboard</Button>
+		</div>
+	{:else}
+		<h1 class="text-2xl">Error</h1>
+
+		<p>{$page.error?.message}</p>
+	{/if}
+</main>

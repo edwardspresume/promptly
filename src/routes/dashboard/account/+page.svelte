0d575d8d62ef50914c5f200f7f294579ec4360b9
Edwards Presume <edwardspresume@gmail.com>
2023-09-21 14:07:41 -0400

    feat(account page): add account page to the dashboard
    - Added a new file `+page.server.ts` to handle server-side loading of the account page.
    - Added a new file `+page.svelte` to define the account page component.

---
+++ src/routes/dashboard/account/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	import { userProfileStore } from '$dashboardStores/userProfileStore';
+
+	import PageSubHeader from '$dashboardComponents/account/PageSubHeader.svelte';
+	import Icon from '$globalComponents/Icon.svelte';
+	import Button from '$globalComponents/ui/button/button.svelte';
+
+	export let data: PageData;
+</script>
+
+<PageSubHeader heading="Main" subheading="Your account information" />
+
+<section>
+	<div>
+		<h3>Email</h3>
+		<p class="text-muted-foreground">{$userProfileStore?.email}</p>
+	</div>
+
+	<form class="flex gap-8 mt-8">
+		<Button formaction="?/exportData" type="button" class="gap-2">
+			<Icon name="download" />
+			<span>Export Data</span>
+		</Button>
+
+		<Button formaction="?/deleteAccount" type="button" variant="destructive" class="gap-2">
+			<Icon name="delete" />
+			<span>Delete Account</span>
+		</Button>
+	</form>
+</section>

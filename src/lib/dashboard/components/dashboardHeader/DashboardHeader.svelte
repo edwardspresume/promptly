049178597462f42738d8d2486912482d504e10a4
Edwards Presume <edwardspresume@gmail.com>
2023-09-22 13:09:13 -0400

    chore(DashboardHeader.svelte): add MainNav component to the DashboardHeader to display navigation links on small screens chore(DashboardHeader.svelte): update DashboardHeaderLinks component to accept a custom class for styling flexibility

--- src/lib/dashboard/components/dashboardHeader/DashboardHeader.svelte
+++ src/lib/dashboard/components/dashboardHeader/DashboardHeader.svelte
@@ -2,6 +2,7 @@
 	import { RoutePaths } from '$globalTypes';
 
 	import Logo from '$globalComponents/mainHeader/Logo.svelte';
+	import MainNav from '$globalComponents/mainHeader/MainNav.svelte';
 	import ThemeToggleBtn from '$globalComponents/mainHeader/ThemeToggleBtn.svelte';
 	import AccountDropdownMenu from './AccountPopUpMenu.svelte';
 	import DashboardHeaderLinks from './DashboardHeaderLinks.svelte';
@@ -9,16 +10,18 @@
 
 <header class="z-50 py-2 border-b">
 	<div class="container flex items-center justify-between max-w-6xl">
-		<nav class="flex items-center gap-2">
-			<Logo href={RoutePaths.DASHBOARD_PROMPTS} linkTitle="Go to prompts page" />
-		</nav>
+		<Logo href={RoutePaths.DASHBOARD_PROMPTS} linkTitle="Go to prompts page" />
 
-		<DashboardHeaderLinks />
+		<DashboardHeaderLinks class="hidden sm:flex" />
 
-		<aside class="flex items-center gap-2">
+		<div class="flex gap-2">
 			<ThemeToggleBtn />
 
 			<AccountDropdownMenu />
-		</aside>
+
+			<MainNav class="sm:hidden">
+				<DashboardHeaderLinks class="grid gap-5" />
+			</MainNav>
+		</div>
 	</div>
 </header>
--- src/lib/dashboard/components/dashboardHeader/DashboardHeaderLinks.svelte
+++ src/lib/dashboard/components/dashboardHeader/DashboardHeaderLinks.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	import { cn } from '$globalUtils';
 
 	import { dashboardLinks } from '$dashboardNavLinks';
-
 	import Button from '$globalComponents/ui/button/button.svelte';
+
+	let className: HTMLAttributes<HTMLUListElement>['class'] = undefined;
+	export { className as class };
 </script>
 
-<ul class="flex">
+<ul class={cn('flex gap-2', className)}>
 	{#each dashboardLinks.mainNav as { href, title } (title)}
 		{@const isCurrentPage = $page.url.pathname === href ? 'page' : undefined}
 
 		<li>
 			<Button
 				{href}
-				variant="ghost"
+				variant="outline"
 				aria-current={isCurrentPage}
-				class="px-2 py-1 h-fit {isCurrentPage ? 'text-primary brightness-125' : ''}"
+				class="w-full sm:py-1 sm:px-2 sm:h-fit {isCurrentPage ? 'text-primary' : ''}"
 			>
 				{title}
 			</Button>

640c13664ee701318847ba6825619590a29ec349
Edwards Presume <edwardspresume@gmail.com>
2023-09-23 08:32:11 -0400

    fix(PageLoadingSpinner.svelte): change script tag language to "ts" to indicate TypeScript feat(PageLoadingSpinner.svelte): only show the spinner if its taking longer than 1s to load fix(isNavigatingStore.ts): import Writable type from 'svelte/store' and explicitly type isNavigatingStore as Writable<boolean> for better type safety

--- src/lib/global/components/general/PageLoadingSpinner.svelte
+++ src/lib/global/components/general/PageLoadingSpinner.svelte
@@ -1,10 +1,29 @@
-<script>
+<script lang="ts">
 	import { isNavigatingStore } from '$globalStores/isNavigatingStore';
+	import { onDestroy } from 'svelte';
 
 	import Icon from '$globalComponents/Icon.svelte';
+
+	let showSpinner = false;
+	let timeoutId: number | NodeJS.Timeout;
+
+	const unsubscribe = isNavigatingStore.subscribe((isNavigating) => {
+		clearTimeout(timeoutId);
+
+		if (isNavigating) {
+			timeoutId = setTimeout(() => (showSpinner = true), 1000);
+		} else {
+			showSpinner = false;
+		}
+	});
+
+	onDestroy(() => {
+		clearTimeout(timeoutId);
+		unsubscribe();
+	});
 </script>
 
-{#if $isNavigatingStore}
+{#if showSpinner}
 	<div class="grid w-full h-full place-content-center">
 		<Icon name="loading-spinner" size={60} class="animate-spin" />
 	</div>

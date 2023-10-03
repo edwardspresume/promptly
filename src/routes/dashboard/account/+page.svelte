5cfb414ae55081802eb7f92c9301a1f226e8402e
Edwards Presume <edwardspresume@gmail.com>
2023-10-03 17:32:53 -0400

    feat(account page): add support for displaying success message and form submission for Stripe checkout feat(account page): set form action to Stripe checkout session URL refactor(account page): improve code readability by using conditional rendering and extracting status messages into a constant object

--- src/routes/dashboard/account/+page.svelte
+++ src/routes/dashboard/account/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import type { PageData } from './$types';
 
 	import { userProfileStore } from '$dashboardStores/userProfileStore';
 	import { userTagsStore } from '$dashboardStores/userTagsStore';
 	import { exportUserData } from '$dashboardUtils/exportUserData';
 
+	import { page } from '$app/stores';
 	import PageSubHeader from '$dashboardComponents/account/PageSubHeader.svelte';
-
 	import Icon from '$globalComponents/Icon.svelte';
+	import SubmitButton from '$globalComponents/form/SubmitButton.svelte';
 	import Button from '$globalComponents/ui/button/button.svelte';
 
 	export let data: PageData;
@@ -20,12 +22,12 @@
 
 	const statusMessages: Record<string, { title: string; message: string; buttonText: string }> = {
 		trialing: {
-			title: 'Welcome to your trial period!',
+			title: 'Enjoy Your Free Trial!',
 			message: 'Experience all the features we offer',
 			buttonText: 'Upgrade Now'
 		},
 		paused: {
-			title: 'Your trial has ended',
+			title: 'Your Free Trial Has Ended',
 			message: 'Please upgrade to continue enjoying our services',
 			buttonText: 'Purchase Pro Plan'
 		},
@@ -35,20 +37,45 @@
 			buttonText: 'Reactivate Subscription'
 		}
 	};
+
+	$: isLoading = false;
+	$: isStripeCheckoutSuccess = $page.url.searchParams.has('success');
 </script>
 
 <PageSubHeader heading="Main" subheading="Your account information" />
 
-{#if $userProfileStore?.subscriptionStatus !== 'active'}
-	{@const currentStatus = statusMessages[$userProfileStore?.subscriptionStatus]}
+{#if $userProfileStore?.subscriptionStatus !== 'active' || isStripeCheckoutSuccess}
 	<section aria-label="Account Status" class="pb-8 mb-6 border-b">
-		<article class="grid gap-4 p-4 rounded bg-accent">
-			<h3>{currentStatus.title}</h3>
-			<p>{currentStatus.message}</p>
+		<article
+			class="grid gap-4 p-4 rounded {isStripeCheckoutSuccess ? 'bg-green-600' : 'bg-accent'}"
+		>
+			{#if isStripeCheckoutSuccess}
+				<h3>
+					Your subscription has been activated! You can now enjoy all the features of the Pro plan!
+				</h3>
+			{:else if $userProfileStore?.subscriptionStatus !== 'active'}
+				{@const currentStatus = statusMessages[$userProfileStore?.subscriptionStatus || 'trialing']}
 
-			<Button class="gap-2 w-fit">
-                <Icon name='crown' />
-                {currentStatus.buttonText}</Button>
+				<h3>{currentStatus?.title}</h3>
+				<p>{currentStatus?.message}</p>
+
+				<form
+					action="?/stripeCheckout"
+					method="POST"
+					use:enhance={() => {
+						isLoading = true;
+						return async ({ update }) => {
+							await update();
+							isLoading = false;
+						};
+					}}
+				>
+					<SubmitButton class="px-4 w-fit" disabled={isLoading}>
+						<Icon name="crown" />
+						{currentStatus?.buttonText}
+					</SubmitButton>
+				</form>
+			{/if}
 		</article>
 	</section>
 {/if}

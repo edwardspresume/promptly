1f9864c98686eb960c18cd51906af370c4a9f846
Edwards Presume <edwardspresume@gmail.com>
2023-10-03 18:45:36 -0400

    feat(dashboard): add onMount hook to subscribe to supabase changes and invalidate navigation when subscription status changes

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { invalidateAll } from '$app/navigation';
+	import { onMount } from 'svelte';
 	import type { LayoutData } from './$types';
 
 	import { SvelteToast } from '@zerodevx/svelte-toast';
@@ -18,10 +20,10 @@
 	export let data: LayoutData;
 
 	// Initialize the data
-	let { session, userProfile, userTags, feedbackForm } = data;
+	let { session, supabase, userProfile, userTags, feedbackForm } = data;
 
 	// Update the data if it changes
-	$: ({ session, userProfile, userTags, feedbackForm } = data);
+	$: ({ session, supabase, userProfile, userTags, feedbackForm } = data);
 
 	// Initialize or update user and store data based on the session.
 
@@ -33,6 +35,29 @@
 		userProfileStore.set(userProfile ?? null);
 		userTagsStore.set(userTags ?? []);
 	}
+
+	onMount(async () => {
+		if ($userProfileStore) {
+			await supabase
+				.channel('profiles_table_DB_changes')
+				.on(
+					'postgres_changes',
+					{
+						event: 'UPDATE',
+						schema: 'public',
+						table: 'profiles_table'
+					},
+					(payload) => {
+						const newSubscription_status = payload.new.subscription_status;
+
+						if (newSubscription_status !== $userProfileStore?.subscriptionStatus) {
+							invalidateAll();
+						}
+					}
+				)
+				.subscribe();
+		}
+	});
 </script>
 
 <div class="h-[100dvh] flex flex-col">

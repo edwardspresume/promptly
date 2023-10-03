d8582caac347342d9e75cecd1bea5ff448a280e3
Edwards Presume <edwardspresume@gmail.com>
2023-10-03 17:37:09 -0400

    style(dashboard/error.svelte): add 'text-center' class to the main element to center align the content

--- src/routes/dashboard/+error.svelte
+++ src/routes/dashboard/+error.svelte
@@ -6,7 +6,7 @@
 	import Button from '$globalComponents/ui/button/button.svelte';
 </script>
 
-<main class="grid h-full gap-3 justify-items-center place-content-center">
+<main class="grid h-full gap-3 text-center justify-items-center place-content-center">
 	{#if $page.status === 401}
 		<h1 class="text-2xl">Authentication required</h1>
 

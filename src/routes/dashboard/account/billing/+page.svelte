411f3cac2133f4ba8d57bae6f84b97f1035585b3
Edwards Presume <edwardspresume@gmail.com>
2023-10-03 19:51:47 -0400

    feat(billing page): add new billing page component

---
+++ src/routes/dashboard/account/billing/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	import PageSubHeader from '$dashboardComponents/account/PageSubHeader.svelte';
+
+	export let data: PageData;
+</script>
+
+<PageSubHeader heading="Billing" subheading="Your billing information" />

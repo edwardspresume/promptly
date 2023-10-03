1bd9804092b21f6c41024a56ec848b8dad91cbc9
Edwards Presume <edwardspresume@gmail.com>
2023-10-03 17:34:13 -0400

    feat(account/+page.server.ts): add support for stripe checkout functionality to allow users to subscribe to the pro plan

--- src/routes/dashboard/account/+page.server.ts
+++ src/routes/dashboard/account/+page.server.ts
@@ -1,6 +1,16 @@
-import type { PageServerLoad } from './$types';
+import { PUBLIC_PRO_PLAN_PRICE_ID } from '$env/static/public';
+import { redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+
+import { drizzleClient } from '$databaseDir/drizzleClient.server';
+import { eq } from 'drizzle-orm';
+
+import { stripeClient } from '$lib/global/stripe.server';
 
 import { getUserPrompts } from '$databaseDir/databaseUtils.server';
+import { profilesTable } from '$databaseDir/schema';
+import { RoutePaths } from '$globalTypes';
+import { logError } from '$globalUtils';
 
 export const load = (async ({ parent }) => {
 	const { session } = await parent();
@@ -9,3 +19,51 @@ export const load = (async ({ parent }) => {
 
 	return { userPrompts };
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	stripeCheckout: async ({ url, locals: { getSession } }) => {
+		const userId = (await getSession())?.user.id;
+
+		if (!userId) return;
+
+		let stripeCustomerId: string | null | undefined = null;
+		let stripeCheckOutURL: string | null = '';
+
+		try {
+			const userProfile = await drizzleClient.query.profilesTable.findFirst({
+				where: eq(profilesTable.id, userId),
+
+				columns: {
+					stripeCustomerId: true
+				}
+			});
+
+			stripeCustomerId = userProfile?.stripeCustomerId;
+
+			if (!stripeCustomerId) throw new Error('User does not have a stripe customer id');
+
+			const checkoutSession = await stripeClient.checkout.sessions.create({
+				mode: 'subscription',
+				customer: stripeCustomerId,
+				client_reference_id: stripeCustomerId,
+				payment_method_types: ['card', 'cashapp', 'paypal'],
+
+				line_items: [
+					{
+						price: PUBLIC_PRO_PLAN_PRICE_ID,
+						quantity: 1
+					}
+				],
+
+				success_url: `${url.origin}${RoutePaths.DASHBOARD_ACCOUNT}?success=true`,
+				cancel_url: `${url.origin}${RoutePaths.DASHBOARD_ACCOUNT}?canceled=true`
+			});
+
+			stripeCheckOutURL = checkoutSession.url;
+		} catch (error) {
+			logError(error, 'Error during Stripe operations', { stripeCustomerId });
+		}
+
+		if (stripeCheckOutURL) throw redirect(303, stripeCheckOutURL);
+	}
+};

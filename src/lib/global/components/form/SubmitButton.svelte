1ac88b91184792c5b66ce784f2653aa383109f9c
Edwards Presume <edwardspresume@gmail.com>
2023-10-04 17:55:26 -0400

    feat(SubmitButton.svelte): add buttonVariant prop to Button component to allow customization of button variant

--- src/lib/global/components/form/SubmitButton.svelte
+++ src/lib/global/components/form/SubmitButton.svelte
@@ -5,6 +5,7 @@
 	import { cn } from '$globalUtils';
 
 	import Icon from '$globalComponents/Icon.svelte';
+	import type { Variant } from '$globalComponents/ui/button';
 	import Button from '$globalComponents/ui/button/button.svelte';
 
 	let className: HTMLButtonAttributes['class'] = undefined;
@@ -13,12 +14,14 @@
 	export { className as class };
 	export let disabled: boolean = false;
 	export let showSpinner: boolean | null = null;
+	export let buttonVariant: Variant = 'default';
 </script>
 
 <Button
 	{size}
 	{disabled}
 	type="submit"
+	variant={buttonVariant}
 	aria-disabled={disabled}
 	class={cn('font-bold flex gap-2 items-center w-full', className)}
 >

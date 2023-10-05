83344cc9db16c5e843fd93012ef290cbb8e1830e
Edwards Presume <edwardspresume@gmail.com>
2023-10-05 11:37:20 -0400

    refactor(MarketingHeaderLinks.svelte): remove unnecessary conditional rendering based on isLoggedIn variable feat(+layout.server.ts): add server-side layout load function to redirect authenticated users to the dashboard prompts page

--- src/lib/marketing/components/marketingHeader/MarketingHeaderLinks.svelte
+++ src/lib/marketing/components/marketingHeader/MarketingHeaderLinks.svelte
@@ -1,27 +1,21 @@
 <script>
-	import { page } from '$app/stores';
-
 	import { marketingLinks } from '$marketingNavLinks';
 
 	import ThemeToggleBtn from '$globalComponents/mainHeader/ThemeToggleBtn.svelte';
 	import Button from '$globalComponents/ui/button/button.svelte';
-
-	$: isLoggedIn = $page.data.session?.user;
 </script>
 
 <ul class="grid items-center gap-5 sm:gap-2 sm:flex">
 	{#each marketingLinks as { title, href } (title)}
-		{#if !isLoggedIn || !href.includes('auth')}
-			<li>
-				<Button
-					{href}
-					variant={title === 'Dashboard' ? 'default' : 'outline'}
-					class="w-full sm:py-1 sm:px-2 sm:h-fit"
-				>
-					{title}
-				</Button>
-			</li>
-		{/if}
+		<li>
+			<Button
+				{href}
+				variant={title === 'Dashboard' ? 'default' : 'outline'}
+				class="w-full sm:py-1 sm:px-2 sm:h-fit"
+			>
+				{title}
+			</Button>
+		</li>
 	{/each}
 
 	<li>
---
+++ src/routes/(marketing)/+layout.server.ts
@@ -0,0 +1,10 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+
+import { RoutePaths } from '$globalTypes';
+
+export const load = (async ({ parent }) => {
+	const { session } = await parent();
+
+	if (session) throw redirect(303, RoutePaths.DASHBOARD_PROMPTS);
+}) satisfies LayoutServerLoad;

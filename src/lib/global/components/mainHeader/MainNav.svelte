bb86b318b32778d757098d44cead0f0ac636fd7b
Edwards Presume <edwardspresume@gmail.com>
2023-10-08 13:16:02 -0400

    fix(MainNav.svelte): add z-index to main nav to ensure it appears above other elements

--- src/lib/global/components/mainHeader/MainNav.svelte
+++ src/lib/global/components/mainHeader/MainNav.svelte
@@ -14,7 +14,7 @@
 <nav
 	id="main-nav"
 	class={cn(
-		`absolute top-0 left-0 px-2 w-full h-full mt-[3.625rem] pt-10 bg-background transition-transform duration-300 sm:relative sm:w-fit sm:-translate-x-0 sm:bg-transparent sm:pt-0 sm:mt-0 ${
+		`absolute z-10 top-0 left-0 px-2 w-full h-full mt-[3.625rem] pt-10 bg-background transition-transform duration-300 sm:relative sm:w-fit sm:-translate-x-0 sm:bg-transparent sm:pt-0 sm:mt-0 ${
 			!isMainNavMenuOpen ? '-translate-x-full' : ''
 		}`,
 		className

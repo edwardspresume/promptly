7bb70e2b343df0186cdf43dcf4e619ba4d807ba4
Edwards Presume <edwardspresume@gmail.com>
2023-10-09 17:08:07 -0400

    refactor(+page.server.ts): remove unheeded code since the settings are already configured

--- src/routes/dashboard/account/billing/+page.server.ts
+++ src/routes/dashboard/account/billing/+page.server.ts
@@ -16,37 +16,12 @@ export const actions: Actions = {
 		let billingPortalURL: string | null = '';
 
 		try {
-			const billingPortalConfig = await stripeClient.billingPortal.configurations.create({
-				business_profile: {
-					headline: 'Manage your Promptly billing settings'
-				},
-
-				features: {
-					invoice_history: { enabled: true },
-					payment_method_update: { enabled: true },
-
-					customer_update: {
-						allowed_updates: ['name', 'phone', 'address', 'tax_id'],
-						enabled: true
-					},
-
-					subscription_cancel: {
-						enabled: true,
-						cancellation_reason: {
-							enabled: true,
-							options: ['customer_service', 'missing_features', 'too_expensive', 'unused']
-						}
-					}
-				}
-			});
-
 			const stripeCustomerId = await getStripeCustomerId(userId);
 
 			if (!stripeCustomerId) throw new Error('User does not have a stripe customer id');
 
 			const customerBillingPortal = await stripeClient.billingPortal.sessions.create({
 				customer: stripeCustomerId,
-				configuration: billingPortalConfig.id,
 				return_url: `${url.origin}${RoutePaths.DASHBOARD_BILLING}`
 			});
 

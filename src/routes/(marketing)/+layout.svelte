008df0352c013cc78917205e46b41660c247673b
Edwards Presume <edwardspresume@gmail.com>
2023-10-09 16:49:55 -0400

    refactor(layout.svelte): remove unused MetaTags import style(layout.svelte): move background image styling from inline style

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -1,27 +1,17 @@
 <script>
-	import MetaTags from '$globalComponents/MetaTags.svelte';
 	import PageLoadingSpinner from '$globalComponents/general/PageLoadingSpinner.svelte';
 	import MarketingHeader from '$marketingComponents/marketingHeader/MarketingHeader.svelte';
 </script>
-
-
 
 <div class="h-[100dvh] flex flex-col">
 	<MarketingHeader />
 
 	<PageLoadingSpinner>
-		<main class="grid flex-1 px-3 place-items-center">
+		<main
+			class="grid flex-1 px-3 bg-center bg-cover place-items-center bg-blend-darken bg-[rgb(0,0,0,0.86)]"
+			style="background-image: url('hero.webp');"
+		>
 			<slot />
 		</main>
 	</PageLoadingSpinner>
 </div>
-
-<style>
-	main {
-		background-image: url('hero.webp');
-		background-size: cover;
-		background-position: center;
-		background-blend-mode: darken;
-		background-color: rgba(0, 0, 0, 0.86);
-	}
-</style>

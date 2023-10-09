acb3c59d1dd28f69d1d2c2cbf08dc90a0054913d
Edwards Presume <edwardspresume@gmail.com>
2023-10-09 11:45:08 -0400

    chore(+page.svelte): update text and styling in the marketing page feat(+page.svelte): add a new message about a Chrome Extension in the pipeline to keep users informed

--- src/routes/(marketing)/+page.svelte
+++ src/routes/(marketing)/+page.svelte
@@ -10,9 +10,13 @@
 		<span class="text-[#7C3AED]">Optimize Your AI Interactions</span> with a Personalized Prompt Library
 	</h1>
 
-	<p class="mt-6 mb-10 text-xl text-[#9CA3AF]">
-		The central place to save, find, share, and manage your prompts.
+	<p class="mt-6 mb-9 text-xl text-[#9CA3AF]">
+		The central place to save, copy, find, share, and manage your AI prompts
 	</p>
 
-	<DashboardLink text="Try it for free" class="text-xl" />
+	<DashboardLink text="Try it for free" class="text-xl font-bold" />
+
+	<p class="p-2 px-3 mx-auto mt-10 font-bold text-white bg-black rounded w-fit">
+		Chrome Extension in the Pipeline — Stay Tuned!
+	</p>
 </div>
